61e932177307354ae781000eb69841a91811585d
Kasper Fabæch Brandt <poizan42@poizan.dk>
2012-01-06 00:49:16 +0100

    Initial commit

---
+++ MsvcrtToMsvcr100/MsvcrtToMsvcr100.h
@@ -0,0 +1,8 @@
+// MsvcrtToMsvcr100.h : main header file for the MsvcrtToMsvcr100 DLL
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
